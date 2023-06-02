31d2496ba7506242ee5ec767709bba056b9803c5
yeet <aniruddh.imp12@gmail.com>
2023-06-02 21:36:28 +0530

    composed the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import { Admin, Resource } from "react-admin";
 import { dataProvider } from './dataProvider';
-import { authProvider } from './authProvider';
+import { UserList } from "./users";
+
 
 export const App = () => (
-    <Admin
-        dataProvider={dataProvider}
-		authProvider={authProvider}
-	>
-        
-    </Admin>
+  <Admin dataProvider={dataProvider}>
+  <Resource name="users" list={UserList} />  </Admin>
 );
-
-    
